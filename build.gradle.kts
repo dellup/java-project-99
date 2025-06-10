6deed0adbae0bf32a93cf43ac636152bb92c2b49
Danil Zenin <zenindanil2000@gmail.com>
2025-06-10 12:10:02 +0300

    add application plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	java
 	checkstyle
+	id("application")
 	id("org.springframework.boot") version "3.5.0"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.asciidoctor.jvm.convert") version "3.3.2"
