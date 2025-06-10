886ffbc868b9929ddbd6f91b128e75b8213861ce
Danil Zenin <zenindanil2000@gmail.com>
2025-06-10 12:10:56 +0300

    add application plugin

--- src/main/java/hexlet/code/demo/controller/WelcomeController.java
+++ src/main/java/hexlet/code/demo/controller/WelcomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/welcome")
 public class WelcomeController {
     @GetMapping("")
+
     public String index() {
         return "Welcome to Spring";
     }
